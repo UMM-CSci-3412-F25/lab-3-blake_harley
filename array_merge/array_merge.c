7f645868b7c8b12a36a80c55977e76480590b839
Harley Hannahs <hanna537@morris.umn.edu>
2025-09-23 22:30:28 -0500

    starting array_merge
    array_merge currently brings all arrays into one unsorted array with duplicates

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,7 +1,42 @@
 #include "array_merge.h"
 #include "../mergesort/mergesort.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
-  return sizes;
+    int total_size = 0;
+    for (int i = 0; i < num_arrays; ++i) {
+        total_size = total_size + sizes[i];
+    }
+
+    int* merged_array = malloc(total_size * sizeof(int));
+
+    int currIndex = 0;
+    for (int i = 0; i < num_arrays; ++i) {
+        for (int j = 0; j < sizes[i]; ++j) {
+            merged_array[currIndex++] = values[i][j];
+        }
+    }
+
+    return merged_array;
+}
+
+int main(void) {
+    int num_arrays = 3;
+    int sizes[] = {4, 2, 5};
+    int a[] = {3, 2, 0, 5};
+    int b[] = {8, 9};
+    int c[] = {6, 3, 2, 0, 5};
+    int* values[] = {a, b, c};
+
+    int* result = array_merge(num_arrays, sizes, values);
+    
+    int total_size = 4 + 2 + 5;
+    for (int i = 0; i < total_size; ++i) {
+        printf("%d ", result[i]);
+    }
+    // printf("\n");
+
+    free(result);
+    return 0;
 }
