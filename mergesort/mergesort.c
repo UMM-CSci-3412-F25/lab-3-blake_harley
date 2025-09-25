29ea6cbc206ea2fbd6da23f597601f3a733418db
Blake Thompson <Thom9170@morris.umn.edu>
2025-09-25 11:05:59 -0500

    Small updates/bug fixes

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,13 +1,42 @@
 #include "mergesort.h"
 
-void mergesort(int size, int values[]) {
-	if (size > 1 ) {
-		merge_recursive(values, 0, size - 1);
+
+static void merge(int values[], int left, int mid, int right){
+	int node1 = mid - left + 1;
+	int node2 = right - mid;
+	
+	int* L = malloc (void*) (node1 * sizeof(int));
+	int* R = malloc (void*) (node2 * sizeof(int));
+
+	for (int i = 0; i < node1; i++) {
+		L[i] = values[left + 1];
 	}
+
+	for (int j = 0; j < node2; j++) {
+		R[j] = values[mid + 1 + j];
+	}
+	
+	int i = 0, j = 0, k = left;
+
+	while ( i < node1 && j < node2 ){
+		if(L[i] <= R[j]){
+			values[k++] = L[i++];
+		} else {
+			values[k++] = R[j++];
+		}
+	}
+
+	while (i < node1) {
+		values[k++] = L[i++];
+	}
+	while (j < node2) {
+		values[k++] = R[j++];
+	}
+	
+
 }
 
 static void merge_recursive(int values[], int left, int right){
-	
 	
 	if (left < right) {
 		int mid = (left + right) / 2;
@@ -17,38 +46,9 @@ static void merge_recursive(int values[], int left, int right){
 	}
 }
 
-static void merge(int values[], int left, int mid, int right){
-	int m1 = mid - left + 1;
-	int m2 = right - mid;
-	
-	int L* = malloc(m1 * sizeof(int));
-	int R* = malloc(m2 * sizeof(int));
-
-	for (int i = 0; i < m1; i++) {
-		L[i] = values[left + 1];
+void mergesort(int size, int values[]) {
+	if (size > 1 ) {
+		merge_recursive(values, 0, size - 1);
 	}
-
-	for (int j = 0; j < m2; j++) {
-		R[j] = values[mid + 1 + j];
-	}
-	
-	int i = 0, j = 0, k = left;
-
-	while ( i < m1 && j < m2 ){
-		if(L[i] <= R[j]){
-			values[k++] = L[i++ ]
-		} else {
-			values[k++] = R[j++]
-		}
-	}
-
-	while (i < m1) {
-		values[k++] = L[i++]
-	}
-	while (j < m2) {
-		values[k++] = R[++]
-	}
-	
-
 }
 
