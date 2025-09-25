259393d654a8b049901518b70282e8dd9e574e01
Blake Thompson <Thom9170@morris.umn.edu>
2025-09-25 10:17:09 -0500

    incomplete mergesort

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,12 +1,34 @@
 #include "mergesort.h"
 
 void mergesort(int size, int values[]) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
-  //
-  // Remember that a key goal here is to learn to use
-  // `malloc/calloc` and `free`, so make sure you explicitly
-  // allocate any new arrays that you need, even if you
-  // might not strictly need to.
-  return;
+	if (size > 1 ) {
+		merge_recursive(values, 0, size - 1);
+	}
 }
+
+static void merge_recursive(int values[], int left, int right){
+	if (left < right) {
+		int mid = (left + right) / 2;
+		merge_recursive(values, left, mid);
+		merge_recursive(values, mid + 1, right);
+		merge(values, left, mid, right);
+	}
+
+}
+
+static void merge(int values[], int left, int mid, int right){
+	int m1 = mid - left + 1;
+	int m2 = right - mid;
+
+	int L = malloc(m1 * sizeof(int));
+	int R = malloc(m2 * sizeof(int));
+
+	while ( i < m1 && j < m2 ){
+		if(L[i] <= R[j]){
+		
+		}
+	
+	}
+
+}
+
